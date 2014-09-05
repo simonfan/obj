e1be0689ed954b3478f1728a3d84112608264c94
simonfan <simonxd@gmail.com>
2014-09-05 02:25:52 -0300

    initial

---
+++ demo/main.js
@@ -0,0 +1,8 @@
+define(['obj'], function (Obj) {
+
+    var msg = 'Main demo of the obj module running!';
+
+	console.log(msg);
+
+	alert(msg);
+});
---
+++ src/index.js
@@ -0,0 +1,30 @@
+//     Obj
+//     (c) sf
+//     Obj is licensed under the MIT terms.
+
+/**
+ * AMD and CJS module.
+ *
+ * @module Obj
+ */
+
+/* jshint ignore:start */
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+/* jshint ignore:end */
+
+define(function (require, exports, module) {
+	'use strict';
+
+	
+	var Subject = require('subject');
+	
+	var Es5Shim = require('es5-shim');
+	
+
+	var Obj = function Obj() {
+
+	};
+
+
+	return Obj;
+});
